d75c0ff57a202a23bcfee5a727f6de52436b0b08
Trevor McCoy <trevorinsd@gmail.com>
2025-02-27 23:03:09 -0500

    Add PDF exporting for form Set up secret key for multiple simultaneous users Make server-side handle empty inputs

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import secrets
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
+from weasyprint import HTML
+from starlette.middleware.sessions import SessionMiddleware
 from typing import List
 from optimizer import best_fit
 
 app = FastAPI()
+
+# Set up secret key to allow for user-specific states
+secret_key = secrets.token_urlsafe(32)
+app.add_middleware(SessionMiddleware, secret_key=secret_key)
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
@@ -20,12 +28,15 @@ async def run_optimizer(
     stocks_quantities: List[int] = Form(...),
     cuts_lengths: List[float] = Form(...),
     cuts_quantities: List[int] = Form(...),
-    cuts_left_wall_angles: List[str] = Form(...),
-    cuts_right_wall_angles: List[str] = Form(...)
+    cuts_left_wall_angles: List[float] = Form(...),
+    cuts_right_wall_angles: List[float] = Form(...)
 ):  
-    # Replace empty values with 180 while keeping valid numbers
-    cuts_left_wall_angles = [float(angle) if angle.strip() else 180 for angle in cuts_left_wall_angles]
-    cuts_right_wall_angles = [float(angle) if angle.strip() else 180 for angle in cuts_right_wall_angles]
+    # Replace empty angle values with 180 while keeping valid numbers
+    cuts_left_wall_angles = [180 if angle == 0 else angle for angle in cuts_left_wall_angles]
+    cuts_right_wall_angles = [180 if angle == 0 else angle for angle in cuts_right_wall_angles]
+
+    # Allow empty stock quantities to represent an essentially infinite quantity (input max is 9999)
+    stocks_quantities = [9999 if quantity == 0 else quantity for quantity in stocks_quantities]
 
     # Sort cuts into stocks leaving as little spare as possible
     result = best_fit(
@@ -39,4 +50,25 @@ async def run_optimizer(
         cuts_right_wall_angles
     )
 
+    # Save the result in the session
+    request.session["result"] = result
+
     return templates.TemplateResponse("result.html", {"request": request, "result": result})
+
+@app.get("/download-pdf/")
+async def download_pdf(request: Request):
+    # Retrieve the result from the session
+    result = request.session.get("result")
+
+    # Render HTML Template
+    template = templates.get_template("result.html")
+    html_content = template.render({"request": request, "result": result})
+
+    # Convert to PDF
+    pdf_bytes = HTML(string=html_content).write_pdf()
+
+    return Response(
+        content=pdf_bytes, 
+        media_type="application/pdf", 
+        headers={"Content-Disposition": "attachment; filename=page.pdf"}
+    )
